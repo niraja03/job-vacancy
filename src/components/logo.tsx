504024305e3a0b4fe0e0b77dc92f2de483623d1f
Niraja K <nirajakurra@gmail.com>
2025-10-22 20:18:33 +0000

    https://png.pngtree.com/png-vector/20210618/ourmid/pngtree-job-vacancy-j

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,27 +1,15 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn("h-8 w-8 text-primary", className)}
-    >
-      <path d="M12 2L12 8" />
-      <path d="M12 14L12 22" />
-      <path d="M17 3L15 5" />
-      <path d="M7 3L9 5" />
-      <path d="M21 15L19 17" />
-      <path d="M3 15L5 17" />
-      <path d="M12 14a4.5 4.5 0 00-4.5 4.5c0 1.93 1.12 3.58 2.7 4.34" />
-      <path d="M12 14a4.5 4.5 0 014.5 4.5c0 1.93-1.12 3.58-2.7 4.34" />
-      <path d="M12 8a4.5 4.5 0 004.5-4.5C16.5 1.57 15.38.08 13.7.66" />
-      <path d="M12 8A4.5 4.5 0 007.5 3.5c0-1.93 1.12-3.58 2.7-4.34" />
-    </svg>
+    <div className={cn("relative h-8 w-12", className)}>
+      <Image
+        src="https://png.pngtree.com/png-vector/20210618/ourmid/pngtree-job-vacancy-join-our-team-vector-png-design-png-image_3471566.jpg"
+        alt="Gramin Jobs Connect Logo"
+        layout="fill"
+        objectFit="contain"
+      />
+    </div>
   );
 }
